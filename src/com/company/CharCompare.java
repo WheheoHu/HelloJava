b44db318cec15f721c81ec60c1b57704e4203810
文昊 胡 <hwh19981124@hotmail.com>
2018-09-21 09:53:34 +0800

    CharCompare Finish!

--- src/com/company/CharCompare.java
+++ src/com/company/CharCompare.java
@@ -3,7 +3,19 @@ import  java.util.Scanner;
 
 public class CharCompare {
     public static void main(String[] args) {
+        Scanner ip_char=new Scanner(System.in);
 
+        char[] Min_char=ip_char.nextLine().toCharArray();
+        char min=Min_char[0];
+        for (int i = 0; i < Min_char.length; i++) {
+            if (Min_char[i]=='#'){
+                break;
+            }
+            else{
+                if (Min_char[i]<min)min=Min_char[i];
+            }
+        }
+        System.out.println(min);
     }
 }
 
