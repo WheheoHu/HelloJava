6e4b38b5966ca0ba4531127f836a8bfefdfa3245
文昊 胡 <hwh19981124@hotmail.com>
2018-09-21 10:25:16 +0800

    Add Monthday

---
+++ src/com/company/MonthDay.java
@@ -0,0 +1,40 @@
+package com.company;
+import java.util.Scanner;
+
+public class MonthDay {
+    public static void main(String[] args) {
+        Scanner Ipmonth_Int=new Scanner(System.in);
+        int month=Ipmonth_Int.nextInt();
+        while (month<1||month>12){
+            System.out.println("Please input right month!");
+            month=Ipmonth_Int.nextInt();
+        }
+        //int day=new Days(month);
+        System.out.println(new Days().DaysOP(month));
+    }
+}
+class Days{
+    private int days=0;
+    Days(){
+    }
+    public int DaysOP(int month){
+
+        switch(month){
+            case 1:days= 31;break;
+            case 2:days= 28;break;
+            case 3:days= 31;break;
+            case 4:days= 30;break;
+            case 5:days= 31;break;
+            case 6:days= 30;break;
+            case 7:days= 31;break;
+            case 8:days= 31;break;
+            case 9:days= 30;break;
+            case 10:days= 31;break;
+            case 11:days= 30;break;
+            case 12:days= 31;break;
+
+        }
+        return days;
+    }
+
+}
