5092e199ec926fb9c8356523d540695340590ad4
文昊 胡 <hwh19981124@hotmail.com>
2018-09-29 21:10:02 +0800

    Emmm

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,12 +1,27 @@
 package com.company;
 
 public class Student {
+    public static void main(String[] args) {
+        Student_class S_class=new Student_class(2333,213,404,"fool","unknown");
+        System.out.println(S_class.toString());
+    }
+}
+
+class Student_class{
     private int idnumber;
     private int inclass;
     private  String name;
     private  String gender;
     private  int age;
-
+    private  String allimfor;
+    Student_class(){}
+    Student_class(int idnumber,int inclass,int age,String name,String gender){
+        this.name=name;
+        this.age=age;
+        this.gender=gender;
+        this.idnumber=idnumber;
+        this.inclass=inclass;
+    }
     public int getIdnumber(){
         return idnumber;
     }
@@ -29,5 +44,9 @@ public class Student {
 
     public void setAge(int age) {
         this.age = age;
+    }
+    public String toString(){
+        allimfor="gender:"+this.gender+" class:"+this.inclass+" idnumber:"+this.idnumber+" age:"+this.age+" name:"+this.name;
+        return allimfor;
     }
 }
