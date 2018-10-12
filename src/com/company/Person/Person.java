4cd8c2a6917f950b55e246a70bbb8ea97dcd4c52
文昊 胡 <hwh19981124@hotmail.com>
2018-10-12 16:52:22 +0800

    Emmm

--- src/com/company/Person/Person.java
+++ src/com/company/Person/Person.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.company.Person;
 
 public abstract class Person {
     private String name;
--- src/com/company/Person/TestPerson.java
+++ src/com/company/Person/TestPerson.java
@@ -1,11 +1,12 @@
-package com.company;
+package com.company.Person;
+
 
 public class TestPerson {
     public static void main(String[] args) {
         Director Dir=new Director("AAAA",'N',111,100,10,12323,"train station");
         System.out.println(Dir.getTelephone());
         System.out.println(Dir.getSalary());
-        Dir.upSalary(12,100);
+        Dir.upSalary(100,100);
         System.out.println(Dir.getSalary());
     }
 }
