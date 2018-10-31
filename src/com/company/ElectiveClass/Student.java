3a0306b94a03865a6659f363b26a1a12200f8209
文昊 胡 <hwh19981124@hotmail.com>
2018-10-31 13:33:13 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/com/company/Person.java
    #	src/com/company/Person/Person.java
    #	src/com/company/Person/TestPerson.java
    #	src/com/company/TestPerson.java
    #	src/com/company/person/Person.java
    #	src/com/company/person/TestPerson.java

--- src/com/company/ElectiveClass/Student.java
+++ src/com/company/ElectiveClass/Student.java
@@ -1,4 +1,9 @@
 package com.company.ElectiveClass;
 
 public class Student {
+    private int stu_no;
+    private String name;
+    private String sex;
+    private int class_no;
+    private String major;
 }
