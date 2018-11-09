18c6650566fb1551094898b863451471ec8cb0a2
文昊 胡 <hwh19981124@hotmail.com>
2018-11-09 13:39:43 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/com/company/Person.java
    #	src/com/company/Person/Person.java
    #	src/com/company/Person/TestPerson.java
    #	src/com/company/TestPerson.java
    #	src/com/company/person/Person.java
    #	src/com/company/person/TestPerson.java

--- src/com/company/ElectiveClass/Teachers.java
+++ src/com/company/ElectiveClass/Teachers.java
@@ -7,19 +7,21 @@ public class Teachers {
     private String sex;
     private String department;
     private Courses courses;
+
     public Teachers(String teacher_no, String name, String sex, String department) {
         this.teacher_no = teacher_no;
         this.name = name;
         this.sex = sex;
         this.department = department;
     }
+
     public void setCourses(Courses courses) {
         this.courses = courses;
     }
 
     void Approve_Course(Student student) {
         for (int i = 0; i < student.sc.size(); i++) {
-            if (student.sc.get(i).getStu_no()==student.getStu_no()){
+            if (student.sc.get(i).getStu_no().equals(student.getStu_no())) {
                 student.sc.get(i).setApproved(true);
                 break;
             }
@@ -27,9 +29,9 @@ public class Teachers {
         courses.AddStudent(student);
     }
 
-    void Score_Course(Student student,int score) {
+    void Score_Course(Student student, int score) {
         for (int i = 0; i < student.sc.size(); i++) {
-            if (student.sc.get(i).getStu_no()==student.getStu_no()){
+            if (student.sc.get(i).getStu_no() == student.getStu_no()) {
                 student.sc.get(i).setScore(score);
                 break;
             }
