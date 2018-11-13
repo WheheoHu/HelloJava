4761f1d6aee6b72b78232c07cf7e5b27079d29ec
文昊 胡 <hwh19981124@hotmail.com>
2018-11-13 21:54:30 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/com/company/Person.java
    #	src/com/company/Person/Person.java
    #	src/com/company/Person/TestPerson.java
    #	src/com/company/TestPerson.java
    #	src/com/company/person/Person.java
    #	src/com/company/person/TestPerson.java

--- src/com/company/Caculator/Cacula.java
+++ src/com/company/Caculator/Cacula.java
@@ -1,8 +1,34 @@
 package com.company.Caculator;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class Cacula {
-    JFrame maiFrame=new JFrame();
+
+public class Cacula extends JFrame {
+    public Cacula() {
+        this.setBounds(0, 0, 500, 500);
+        Container container = this.getContentPane();
+        JPanel paneltop = new JPanel(new BorderLayout());
+        JPanel panelmid = new JPanel(new GridLayout(4, 4, 10, 10));
+
+        //top
+        paneltop.add(new JTextField("结果"), BorderLayout.EAST);
+        paneltop.add(new JButton("计算"), BorderLayout.WEST);
+        container.add(paneltop, BorderLayout.NORTH);
+        //mid
+        String[] s = "7,8,9,＋,-,*,/,4,5,6,1,2,3,.".split(",");
+        for (int i = 0; i < s.length; i++) {
+            panelmid.add(new JButton(s[i]));
+        }
+        container.add(panelmid, BorderLayout.CENTER);
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new Cacula();
+    }
 
 }
