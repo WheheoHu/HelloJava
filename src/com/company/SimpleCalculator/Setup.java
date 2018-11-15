6edf0c71818f45b79289f064f5c7ce9defd838e7
文昊 胡 <hwh19981124@hotmail.com>
2018-11-15 20:39:04 +0800

    GUI Fin

--- src/com/company/SimpleCalculator/Setup.java
+++ src/com/company/SimpleCalculator/Setup.java
@@ -1,7 +1,14 @@
 package com.company.SimpleCalculator;
 
+import javax.swing.*;
+
 public class Setup {
     public static void main(String[] args) {
+        try{
+            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
         Calculator calculator=new Calculator();
     }
 }
