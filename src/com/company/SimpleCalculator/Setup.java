4a26dc341c8bb7dea49e83aa5e37841a0d26137c
文昊 胡 <hwh19981124@hotmail.com>
2018-11-30 10:24:35 +0800

    Some Useless exceptions

--- src/com/company/SimpleCalculator/Setup.java
+++ src/com/company/SimpleCalculator/Setup.java
@@ -12,13 +12,38 @@ public class Setup {
         }
         try{
         InputStream f=new FileInputStream ("src/com/company/SimpleCalculator/input");
-        f.close();}
+
+        f.close();
+
+        String stringnull=null;
+        System.out.println(stringnull.charAt(0));
+        }
         catch (FileNotFoundException e){
             System.out.println(e.getMessage());
         }
         catch (IOException e){
             System.out.println(e.getMessage());
         }
+        catch(NullPointerException e){
+            System.out.println(e.getMessage());
+        }
+
+        try {String string_1="this is a string";
+            System.out.println(string_1.charAt(20));}
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println(e.getMessage());
+        }
+        catch (StringIndexOutOfBoundsException e){
+            System.out.println(e.getMessage());
+        }
+        try{
+            int num=Integer.parseInt("xixi");
+
+        }
+        catch (NumberFormatException e){
+            System.out.println(e.getMessage());
+        }
+
         Calculator calculator=new Calculator();
     }
 }
