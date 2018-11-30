f08cd9f8bf211ec250532f77db139766ede2ba79
文昊 胡 <hwh19981124@hotmail.com>
2018-11-30 08:09:47 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/com/company/Person.java
    #	src/com/company/Person/Person.java
    #	src/com/company/Person/TestPerson.java
    #	src/com/company/TestPerson.java
    #	src/com/company/person/Person.java
    #	src/com/company/person/TestPerson.java

--- src/com/company/SimpleCalculator/Setup.java
+++ src/com/company/SimpleCalculator/Setup.java
@@ -1,6 +1,7 @@
 package com.company.SimpleCalculator;
 
 import javax.swing.*;
+import java.io.*;
 
 public class Setup {
     public static void main(String[] args) {
@@ -9,7 +10,15 @@ public class Setup {
         }catch(Exception e){
             e.printStackTrace();
         }
-
+        try{
+        InputStream f=new FileInputStream ("src/com/company/SimpleCalculator/input");
+        f.close();}
+        catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+        catch (IOException e){
+            System.out.println(e.getMessage());
+        }
         Calculator calculator=new Calculator();
     }
 }
