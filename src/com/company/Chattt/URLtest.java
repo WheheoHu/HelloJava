dcfb82ac8a5685ea2d9179b430acc87b6c53aa51
文昊 胡 <hwh19981124@hotmail.com>
2018-12-14 11:05:04 +0800

    up

--- src/com/company/Chattt/URLtest.java
+++ src/com/company/Chattt/URLtest.java
@@ -8,8 +8,7 @@ public class URLtest {
     public static void main(String[] args) {
         try {
             URL url1=new URL("http://www.ncut.edu.cn/xxjs1/xxgk.htm");
-            InputStream inputStream=url1.openStream();
-            InputStreamReader inputStreamReader=new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+            InputStream inputStream=url1.openStream();InputStreamReader inputStreamReader=new InputStreamReader(inputStream, StandardCharsets.UTF_8);
             BufferedReader bufferedReader=new BufferedReader(inputStreamReader);
             PrintWriter printWriter=new PrintWriter(new BufferedWriter(new FileWriter("test.html")));
             String str;
@@ -19,6 +18,7 @@ public class URLtest {
             bufferedReader.close();
             printWriter.close();
         }
+
         catch (IOException e){
             e.printStackTrace();
         }
