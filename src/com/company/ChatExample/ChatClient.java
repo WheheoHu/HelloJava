b3ca3ca272f96a9f8d16e6ec5571df9befc6cdd0
文昊 胡 <hwh19981124@hotmail.com>
2018-12-21 14:03:13 +0800

    format code

--- src/com/company/ChatExample/ChatClient.java
+++ src/com/company/ChatExample/ChatClient.java
@@ -10,140 +10,137 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-class ClientSocketThread implements Runnable{
-	@Override
-	public void run() {
-		try {
-			ChatClient.contentText.append("Waiting Connect"+"\n");
-			Socket s=new Socket(ChatClient.ipText.getText(), Integer.parseInt(ChatClient.portText.getText()));
-			
-			
-			ChatClient.m_output=new PrintWriter(s.getOutputStream());
-			ChatClient.m_input=new BufferedReader(new InputStreamReader(
-					s.getInputStream()));
-			ChatClient.inputText.setEnabled(true);
-			String mes;		//来自服务器的信息
-			while(true)
-			{
-				do{
-					mes= ChatClient.m_input.readLine();
-					System.out.println(mes);
-					ChatClient.contentText.append("SERVER:"+mes+"\n");
-				}while(!isEnd(mes.trim()));
-				ChatClient.m_output.println("quit");
-				System.out.println("Connection interruption");
-				ChatClient.m_output.flush();
-				ChatClient.m_output.close();
-				ChatClient.m_input.close();
-				s.close();
-				ChatClient.contentText.append("Connection interruption"+"\n");
-				System.exit(0);
-			}
+class ClientSocketThread implements Runnable {
+    @Override
+    public void run() {
+        try {
+            ChatClient.contentText.append("Waiting Connect" + "\n");
+            Socket s = new Socket(ChatClient.ipText.getText(), Integer.parseInt(ChatClient.portText.getText()));
 
-		} catch (NumberFormatException | IOException e1) {
-			e1.printStackTrace();
-		}
+
+            ChatClient.m_output = new PrintWriter(s.getOutputStream());
+            ChatClient.m_input = new BufferedReader(new InputStreamReader(
+                    s.getInputStream()));
+            ChatClient.inputText.setEnabled(true);
+            String mes;        //来自服务器的信息
+            while (true) {
+                do {
+                    mes = ChatClient.m_input.readLine();
+                    System.out.println(mes);
+                    ChatClient.contentText.append("SERVER:" + mes + "\n");
+                } while (!isEnd(mes.trim()));
+                ChatClient.m_output.println("quit");
+                System.out.println("Connection interruption");
+                ChatClient.m_output.flush();
+                ChatClient.m_output.close();
+                ChatClient.m_input.close();
+                s.close();
+                ChatClient.contentText.append("Connection interruption" + "\n");
+                System.exit(0);
+            }
+
+        } catch (NumberFormatException | IOException e1) {
+            e1.printStackTrace();
+        }
     }
-	private boolean isEnd(String m)
-	{
+
+    private boolean isEnd(String m) {
         return m.equalsIgnoreCase("quit");
-	}
-	
+    }
+
 }
+
 public class ChatClient extends JFrame implements ActionListener {
-	static TextField ipText;
-	static TextField portText;
-	static TextArea contentText;
-	static TextField inputText;
+    static TextField ipText;
+    static TextField portText;
+    static TextArea contentText;
+    static TextField inputText;
 
     static BufferedReader m_input;
-	static PrintWriter m_output;
-	
-	private ChatClient()
-	{
-		super("Chat Client");
+    static PrintWriter m_output;
 
-		setLayout(new FlowLayout());
-		Panel ipPanel=new Panel();
-		ipPanel.setLayout(new FlowLayout());
-		Label ipLabel=new Label("SERVER IP:");
-		ipLabel.setSize(40,40);		
-		ipText=new TextField(40);
-		ipPanel.add(ipLabel);
-		ipPanel.add(ipText);
-		
-		Panel portPanel=new Panel();
-		portPanel.setLayout(new FlowLayout());
-		Label portLabel=new Label("SERVER port:");
-		portLabel.setSize(40,40);		
-		portText=new TextField(10);
-		portPanel.add(portLabel);
-		portPanel.add(portText);
+    private ChatClient() {
+        super("Chat Client");
+
+        setLayout(new FlowLayout());
+        Panel ipPanel = new Panel();
+        ipPanel.setLayout(new FlowLayout());
+        Label ipLabel = new Label("SERVER IP:");
+        ipLabel.setSize(40, 40);
+        ipText = new TextField(40);
+        ipPanel.add(ipLabel);
+        ipPanel.add(ipText);
+
+        Panel portPanel = new Panel();
+        portPanel.setLayout(new FlowLayout());
+        Label portLabel = new Label("SERVER port:");
+        portLabel.setSize(40, 40);
+        portText = new TextField(10);
+        portPanel.add(portLabel);
+        portPanel.add(portText);
 
         Button connectButton = new Button("CONNECT");
-		connectButton.addActionListener(this);
-		
-		Panel contentPanel=new Panel();
-		contentPanel.setLayout(new FlowLayout());
-		Label contentLabel=new Label("CONTENT");
-		contentLabel.setSize(40,40);		
-		contentText=new TextArea();
-		contentPanel.add(contentLabel);
-		contentPanel.add(contentText);
-		
-		
-		Panel inputPanel=new Panel();
-		inputPanel.setLayout(new FlowLayout());
-		Label inputLabel=new Label("INPUT");
-		inputLabel.setSize(40,40);		
-		inputText=new TextField(40);
-		inputPanel.add(inputLabel);
-		inputPanel.add(inputText);
+        connectButton.addActionListener(this);
+
+        Panel contentPanel = new Panel();
+        contentPanel.setLayout(new FlowLayout());
+        Label contentLabel = new Label("CONTENT");
+        contentLabel.setSize(40, 40);
+        contentText = new TextArea();
+        contentPanel.add(contentLabel);
+        contentPanel.add(contentText);
+
+
+        Panel inputPanel = new Panel();
+        inputPanel.setLayout(new FlowLayout());
+        Label inputLabel = new Label("INPUT");
+        inputLabel.setSize(40, 40);
+        inputText = new TextField(40);
+        inputPanel.add(inputLabel);
+        inputPanel.add(inputText);
 
         Button sendButton = new Button("SENT");
-		sendButton.addActionListener(this);
-		
-		add(ipPanel);
-		add(portPanel);
-		add(connectButton);
-		add(contentPanel);
-		add(inputPanel);		
-		add(sendButton);
-		
-		
-		setSize(500,500);
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-	}
-	public static void main(String[] args) {
+        sendButton.addActionListener(this);
 
-		new ChatClient();
-		
+        add(ipPanel);
+        add(portPanel);
+        add(connectButton);
+        add(contentPanel);
+        add(inputPanel);
+        add(sendButton);
 
-	}
-	@Override
-	public void actionPerformed(ActionEvent e) {
 
-		if(e.getActionCommand().equals("CONTENT"))
-		{
-			ConnectServer();
-		}
-		else if(e.getActionCommand().equals("SEND"))
-		{
-			Send();
-		}
-	}
-	private void ConnectServer()
-	{
-		new Thread(new ClientSocketThread()).start();
-	}
+        setSize(500, 500);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-	private void Send()
-	{
-		contentText.append("Client:"+inputText.getText()+"\n");
-		m_output.println(inputText.getText()+"\n");
-		m_output.flush();
-		inputText.setText("");
-	}
+    }
+
+    public static void main(String[] args) {
+
+        new ChatClient();
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if (e.getActionCommand().equals("CONTENT")) {
+            ConnectServer();
+        } else if (e.getActionCommand().equals("SEND")) {
+            Send();
+        }
+    }
+
+    private void ConnectServer() {
+        new Thread(new ClientSocketThread()).start();
+    }
+
+    private void Send() {
+        contentText.append("Client:" + inputText.getText() + "\n");
+        m_output.println(inputText.getText() + "\n");
+        m_output.flush();
+        inputText.setText("");
+    }
 }
--- src/com/company/ChatExample/ChatServer.java
+++ src/com/company/ChatExample/ChatServer.java
@@ -13,150 +13,149 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-class SocketThread implements Runnable{
+class SocketThread implements Runnable {
     @Override
-	public void run() {
-		try {
+    public void run() {
+        try {
             Socket soc = ChatServer.server.accept();
-			ChatServer.contentText.append("Server receive connect");
-			ChatServer.m_output=new PrintWriter(soc.getOutputStream());
-			ChatServer.m_input=new BufferedReader(new InputStreamReader(
-					soc.getInputStream()));
-			ChatServer.m_output.println("connected");
-			ChatServer.m_output.flush();
-			ChatServer.inputText.setEnabled(true);
-			String mes;
-			do{
-				mes=ChatServer.m_input.readLine();
-				ChatServer.contentText.append("Client:"+mes+"\n");
-			}while(!isEnd(mes.trim()));
-			ChatServer.m_output.println("quit");
-			ChatServer.m_output.flush();
-			ChatServer.m_output.close();
-			ChatServer.m_input.close();
-			soc.close();
-			ChatServer.contentText.append("connect finished"+"\n");
-			System.exit(0);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	private boolean isEnd(String m)
-	{
+            ChatServer.contentText.append("Server receive connect");
+            ChatServer.m_output = new PrintWriter(soc.getOutputStream());
+            ChatServer.m_input = new BufferedReader(new InputStreamReader(
+                    soc.getInputStream()));
+            ChatServer.m_output.println("connected");
+            ChatServer.m_output.flush();
+            ChatServer.inputText.setEnabled(true);
+            String mes;
+            do {
+                mes = ChatServer.m_input.readLine();
+                ChatServer.contentText.append("Client:" + mes + "\n");
+            } while (!isEnd(mes.trim()));
+            ChatServer.m_output.println("quit");
+            ChatServer.m_output.flush();
+            ChatServer.m_output.close();
+            ChatServer.m_input.close();
+            soc.close();
+            ChatServer.contentText.append("connect finished" + "\n");
+            System.exit(0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean isEnd(String m) {
         return m.equalsIgnoreCase("quit");
-	}
-	
+    }
+
 }
-public class ChatServer extends JFrame implements ActionListener{
+
+public class ChatServer extends JFrame implements ActionListener {
     private static TextField portText;
-	static TextArea contentText;
-	static TextField inputText;
+    static TextArea contentText;
+    static TextField inputText;
 
     static BufferedReader m_input;
-	static PrintWriter m_output;
-	
-	private ChatServer()
-	{
-		super("Chat Server");
-		setLayout(new FlowLayout());
-		//Choice cb=new Choice();
-		Panel ipPanel=new Panel();
-		ipPanel.setLayout(new FlowLayout());
-		Label ipLabel=new Label("Local IP:");
-		ipLabel.setSize(40,40);
-        TextField ipText = new TextField();
-		ipPanel.add(ipLabel);
-		ipPanel.add(ipText);
-		
-		Panel portPanel=new Panel();
-		portPanel.setLayout(new FlowLayout());
-		Label portLabel=new Label("Local port:");
-		portLabel.setSize(80,40);		
-		portText=new TextField();
-		portPanel.add(portLabel);
-		portPanel.add(portText);
-		
-		try {
-			ipText.setText(InetAddress.getLocalHost().getHostAddress());
-		} catch (UnknownHostException e) {
+    static PrintWriter m_output;
 
-			e.printStackTrace();
-		}
+    private ChatServer() {
+        super("Chat Server");
+        setLayout(new FlowLayout());
+        //Choice cb=new Choice();
+        Panel ipPanel = new Panel();
+        ipPanel.setLayout(new FlowLayout());
+        Label ipLabel = new Label("Local IP:");
+        ipLabel.setSize(40, 40);
+        TextField ipText = new TextField();
+        ipPanel.add(ipLabel);
+        ipPanel.add(ipText);
+
+        Panel portPanel = new Panel();
+        portPanel.setLayout(new FlowLayout());
+        Label portLabel = new Label("Local port:");
+        portLabel.setSize(80, 40);
+        portText = new TextField();
+        portPanel.add(portLabel);
+        portPanel.add(portText);
+
+        try {
+            ipText.setText(InetAddress.getLocalHost().getHostAddress());
+        } catch (UnknownHostException e) {
+
+            e.printStackTrace();
+        }
 
         Button waitForMesButton = new Button("Waiting Massage");
-		waitForMesButton.addActionListener(this);
-		
-		Panel contentPanel=new Panel();
-		contentPanel.setLayout(new FlowLayout());
-		Label contentLabel=new Label("Com");
-		contentLabel.setSize(40,40);		
-		contentText=new TextArea();
-		contentPanel.add(contentLabel);
-		contentPanel.add(contentText);
-		
-		
-		Panel inputPanel=new Panel();
-		inputPanel.setLayout(new FlowLayout());
-		Label inputLabel=new Label("INPUT");
-		inputLabel.setSize(40,40);		
-		inputText=new TextField();
-		inputPanel.add(inputLabel);
-		inputPanel.add(inputText);
+        waitForMesButton.addActionListener(this);
+
+        Panel contentPanel = new Panel();
+        contentPanel.setLayout(new FlowLayout());
+        Label contentLabel = new Label("Com");
+        contentLabel.setSize(40, 40);
+        contentText = new TextArea();
+        contentPanel.add(contentLabel);
+        contentPanel.add(contentText);
+
+
+        Panel inputPanel = new Panel();
+        inputPanel.setLayout(new FlowLayout());
+        Label inputLabel = new Label("INPUT");
+        inputLabel.setSize(40, 40);
+        inputText = new TextField();
+        inputPanel.add(inputLabel);
+        inputPanel.add(inputText);
 
         Button sendButton = new Button("SEND");
-		sendButton.addActionListener(this);
-		
-		add(ipPanel);
-		add(portPanel);
-		add(waitForMesButton);
-		add(contentPanel);
-		add(inputPanel);		
-		add(sendButton);
-		
-		
-		setSize(500,500);
-		setVisible(true);
+        sendButton.addActionListener(this);
+
+        add(ipPanel);
+        add(portPanel);
+        add(waitForMesButton);
+        add(contentPanel);
+        add(inputPanel);
+        add(sendButton);
+
+
+        setSize(500, 500);
+        setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-	}
-	public static void main(String[] args) {
-		new ChatServer();
 
-	}
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		//System.out.println(e.getActionCommand());
-		if(e.getActionCommand().equals("WAITING"))
-		{
-			CreateServer();
-		}
-		else if(e.getActionCommand().equals("SEND"))
-		{
-			Send();
-		}
-	}
-	static ServerSocket server;
-	private void CreateServer()
-	{
-		contentText.append("SERVER STARTED\n");
-		try {
-			server=new ServerSocket(Integer.parseInt(ChatServer.portText.getText()));
-			System.out.println("SERVER STARTED");
+    }
 
-		} catch (NumberFormatException | IOException e1) {
-			e1.printStackTrace();
-		}
+    public static void main(String[] args) {
+        new ChatServer();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //System.out.println(e.getActionCommand());
+        if (e.getActionCommand().equals("WAITING")) {
+            CreateServer();
+        } else if (e.getActionCommand().equals("SEND")) {
+            Send();
+        }
+    }
+
+    static ServerSocket server;
+
+    private void CreateServer() {
+        contentText.append("SERVER STARTED\n");
+        try {
+            server = new ServerSocket(Integer.parseInt(ChatServer.portText.getText()));
+            System.out.println("SERVER STARTED");
+
+        } catch (NumberFormatException | IOException e1) {
+            e1.printStackTrace();
+        }
 
         new Thread(new SocketThread()).start();
-	}
+    }
 
-	private void Send()
-	{
-		contentText.append("SERVER:"+inputText.getText()+"\n");
-		m_output.println(inputText.getText()+"\n");
-		m_output.flush();
-		inputText.setText("");
-	}
-	
+    private void Send() {
+        contentText.append("SERVER:" + inputText.getText() + "\n");
+        m_output.println(inputText.getText() + "\n");
+        m_output.flush();
+        inputText.setText("");
+    }
+
 }
 
